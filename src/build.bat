5488ea893008ae43c9a15b46730c804816cbee1a
LADSsoft <touchstone222@runbox.com>
2022-09-03 05:21:15 -0400

    build first with mingw32-make then rebuild with itself

--- src/build.bat
+++ src/build.bat
@@ -9,13 +9,19 @@
          del /Q ..\include\*.*
          del /Q ..\include\win32\*.*
          del /Q ..\include\sys\*.*
-         omake -DCOMPILER=MS clean -j:%NUMBER_OF_PROCESSORS%
+         del /Q omake.exe
+         mingw32-make -DCOMPILER=MS clean -j:%NUMBER_OF_PROCESSORS%
          set BUILD_PROFILE=MS
          set TESTS=TRUE
          set PARALLEL=%NUMBER_OF_PROCESSORS%
      )
               cd c:\omake\src
               mkdir lib > NUL
+              mingw32-make -j %PARALLEL%
+                  IF %ERRORLEVEL% NEQ 0 (
+                      goto error;
+                  )
+              copy c:\omake\src\omake\omake.exe .\
               omake -j %PARALLEL%
                   IF %ERRORLEVEL% NEQ 0 (
                       goto error;
--- src/config.bat
+++ src/config.bat
@@ -1,3 +1,4 @@
+set PATH=\orangec\bin;\mingw64\w64\bin;%PATH%;\program files\llvm\bin;\cc386\bin
 set ORANGEC=\omake
 set ORANGEC_HOME=omake
 call vcvars32.bat
